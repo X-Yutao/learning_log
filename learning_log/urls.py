fc0ee2741de7a9540bf9e2ffca12130b12b1e784
徐钰涛 <yutao@xuyutaodeMacBook-Pro.local>
2020-02-09 16:10:19 -0400

    user function created

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls import include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^users/', include('users.urls', namespace='users')),
     url(r'', include('learning_logs.urls', namespace='learning_logs')),
-    url(r'^users/', include('users.urls', namespace='users'))
+
 ]
